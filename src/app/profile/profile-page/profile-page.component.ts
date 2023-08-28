39ef7f9602e9a7d818deb8172115881ff254d9a0
phumipat jaipattanasak <j.phumipat09044616987@gmail.com>
2023-08-28 18:37:29 +0700

    1

--- src/app/profile/profile-page/profile-page.component.ts
+++ src/app/profile/profile-page/profile-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink,RouterLinkActive,RouterOutlet],
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss']
 })
